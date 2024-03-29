064e0d2dc94c7bc45df74f6d48bdbc989a383825
Julio Campos <juliocesar5024@gmail.com>
2024-03-29 12:58:04 -0500

    delete view will appear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,9 +14,5 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        
-    }
-
 }
 
